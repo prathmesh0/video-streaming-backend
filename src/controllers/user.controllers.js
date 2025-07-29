3d99dbda0c30dd843f946842f0cf56860bc6c7f2
Prathmesh12 <prathmesh.parab@lauren.co.in>
2025-07-29 16:34:52 +0530

    fix: remove console log from getUserChannelProfile function

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -381,7 +381,7 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
       },
     },
   ]);
-  console.log(channel);
+
   if (!channel?.length) {
     throw new ApiError(404, "Channel does not exist");
   }
